d1177b8d19fec708047bb272ca4d49154c354817
zkep <773683464@qq.com>
2024-02-08 08:24:28 +0800

    add hook update

--- hook/hook.go
+++ hook/hook.go
@@ -13,7 +13,10 @@ var (
 
 type Hook[T any] interface {
 	Register(p T, index ...int) error
+
 	Get(match func(p T) bool) (T, error)
+
+	Update(match func(T) bool, p T) (T, error)
 }
 
 type providerWrap[T any] struct {
@@ -42,7 +45,6 @@ func (h *IHook[T]) Register(provide T, indexes ...int) error {
 			insertIndex = i
 			break
 		}
-		continue
 	}
 	wrap := &providerWrap[T]{provide: provide, index: index}
 	curr := append(append([]*providerWrap[T]{}, h.providers[0:insertIndex]...), wrap)
@@ -57,5 +59,32 @@ func (h *IHook[T]) Get(match func(T) bool) (m T, e error) {
 		}
 	}
 	e = ErrNotMatchProvider
+	return
+}
+
+func (h *IHook[T]) Update(match func(T) bool, provide T) (m T, e error) {
+	if reflect.TypeOf(provide) == nil {
+		e = ErrNilProvide
+		return
+	}
+	h.lock.Lock()
+	defer h.lock.Unlock()
+	index := 0
+	for i, wrap := range h.providers {
+		if match(wrap.provide) {
+			index = i
+			m = wrap.provide
+			break
+		}
+	}
+
+	if reflect.TypeOf(m) == nil {
+		e = ErrNotMatchProvider
+		return
+	}
+
+	wrap := &providerWrap[T]{provide: provide, index: index}
+	h.providers[index] = wrap
+
 	return
 }
--- hook/hook_test.go
+++ hook/hook_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/uaxe/infra/hook"
 )
 
-func Test_Hook(t *testing.T) {
+func Test_HookAny(t *testing.T) {
 	var (
 		defaultHook = &hook.IHook[any]{}
 		Register    = defaultHook.Register
@@ -30,7 +30,7 @@ func Test_Hook(t *testing.T) {
 	}
 }
 
-func Test_HookStruct(t *testing.T) {
+func Test_Hook(t *testing.T) {
 	type Server struct {
 		Addr string
 		Port int
@@ -39,13 +39,19 @@ func Test_HookStruct(t *testing.T) {
 		defaultHook = &hook.IHook[*Server]{}
 		Register    = defaultHook.Register
 		Get         = defaultHook.Get
+		Update      = defaultHook.Update
 	)
-	provide := &Server{
-		Port: 80,
-	}
+
+	provide := &Server{Port: 80}
+
 	if err := Register(provide); err != nil {
 		t.FailNow()
 	}
+
+	if err := Register(&Server{Port: 81}); err != nil {
+		t.FailNow()
+	}
+
 	curr, err := Get(func(s *Server) bool {
 		return provide.Port == s.Port
 	})
@@ -60,12 +66,23 @@ func Test_HookStruct(t *testing.T) {
 		t.FailNow()
 	}
 
-	_, err = Get(func(s *Server) bool {
-		return provide.Port != s.Port
-	})
-
+	_, err = Get(func(s *Server) bool { return provide.Addr == "127.0.0.1" })
 	if !errors.Is(err, hook.ErrNotMatchProvider) {
 		t.Logf("err: %+v", err)
+		t.FailNow()
+	}
+
+	newProvide := &Server{Port: 8000}
+	oldProvide, err := Update(func(s *Server) bool {
+		return provide.Port == s.Port
+	}, newProvide)
+	if err != nil {
+		t.Logf("err: %+v", err)
+		t.FailNow()
+	}
+
+	if oldProvide.Port != provide.Port {
+		t.Logf("oldProvide: %+v", curr)
 		t.FailNow()
 	}
 
