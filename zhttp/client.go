fc15d881a9d9846b0526841f5c0c266259a8a83f
zkep <773683464@qq.com>
2024-02-11 07:21:58 +0800

    update http

--- zhttp/client.go
+++ zhttp/client.go
@@ -2,52 +2,8 @@ package zhttp
 
 import (
 	"net/http"
-	"time"
 )
 
-var DefaultClient = &Client{
-	Client: &http.Client{
-		Transport: Transport(false, nil),
-	},
-}
-
-type (
-	Client struct {
-		*http.Client
-	}
-
-	ClientOption func(c *Client)
-)
-
-func WithTransport(t *http.Transport) ClientOption {
-	return func(c *Client) {
-		c.Transport = t
-	}
-}
-
-func WithCheckRedirect(redirect func(*http.Request, []*http.Request) error) ClientOption {
-	return func(c *Client) {
-		c.CheckRedirect = redirect
-	}
-}
-
-func WithJar(jar http.CookieJar) ClientOption {
-	return func(c *Client) {
-		c.Jar = jar
-	}
-}
-
-func WithTimeout(timeout time.Duration) ClientOption {
-	return func(c *Client) {
-		c.Timeout = timeout
-	}
-}
-
-func NewClient(opts ...ClientOption) *Client {
-	c := &Client{}
-	c.Transport = Transport(false, nil)
-	for _, opt := range opts {
-		opt(c)
-	}
-	return c
+var DefaultClient = &http.Client{
+	Transport: Transport(false, nil),
 }
